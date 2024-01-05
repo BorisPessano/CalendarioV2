a4ef462fd8bc93a62f22395a96e0a6263bf2bcdc
Boris Pessano <borispessano@192.168.1.7>
2024-01-05 11:18:34 -0300

    Upload my branch and little changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,42 @@
+import Link from 'next/link'
+
 const Login = () =>{
     return (
-        <p>Login</p>
+        <section class="bg-gray-50 dark:bg-gray-900">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Clevendario</h1>
+
+                <form class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Mail:</label>
+                        <input type="email" id="Email" name="Email"/>
+                    </div>
+
+                    <div>
+                        <label>Pass:</label>
+                        <input type="password" id="Password" name="Password"/>
+                    </div>
+
+
+                </form>
+            </div>
+            
+            
+            
+              
+                    
+                    
+                   
+
+                    <div class="row d-flex justify-content-center">
+                        <button type="submit" class="btn btn-primary mr-3" onclick="Login()">Ingresar</button>
+                        <button type="button" class="btn btn-success " onclick="register()">Registrarse</button>
+                    </div>
+
+                    <div class="spinner-border text-primary text-center mx-auto" role="status" id="spinner">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+        </section>
     )
 }
 export default Login
