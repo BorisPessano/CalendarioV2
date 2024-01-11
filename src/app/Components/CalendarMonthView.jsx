84355ee17e715ea48c609eb64c7bd4991c8e1af6
virginia-costa <costa.virginia7@gmail.com>
2024-01-11 14:09:03 -0300

    Some changes in calendar

--- src/app/Components/CalendarMonthView.jsx
+++ src/app/Components/CalendarMonthView.jsx
@@ -1,33 +1,68 @@
 import React from 'react';
-import styles from './CalendarMonthView.module.css'; 
+import styles from './CalendarMonthView.module.css';
 
 const CalendarMonthView = ({ year, month, feriados }) => {
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const dayOfWeekLabels = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'];
 
   const isWeekend = (day) => {
     const dayOfWeek = new Date(year, month, day).getDay();
-    return dayOfWeek === 0 || dayOfWeek === 6;
+    return dayOfWeek === 6 || dayOfWeek === 0;
   };
 
   const isFeriado = (day) => {
     const formattedDate = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
-    return feriados.some((feriado) => feriado.start === formattedDate);
+    const feriado = feriados.find((feriado) => feriado.start === formattedDate);
+    return feriado ? feriado : null;
   };
 
   const renderDay = (day) => {
-    const weekendStyle = isWeekend(day) ? styles.weekend : {}; 
-    const feriadoStyle = isFeriado(day) ? { color: 'red' } : {};
+    const weekendStyle = isWeekend(day) ? styles.weekend : {};
+    const feriado = isFeriado(day);
+    const feriadoStyle = feriado ? { color: 'white', backgroundColor: 'blue' } : {};
 
     return (
       <div key={day} className={`${styles.day} ${weekendStyle}`} style={feriadoStyle}>
-        {day}
+        {feriado ? feriado.title : day}
       </div>
     );
   };
 
+  const renderDayLabels = () => {
+    return (
+      <div className={styles.week}>
+        {dayOfWeekLabels.map((label) => (
+          <div key={label} className={styles.weekdayLabel}>
+            {label}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const renderWeek = (startDay) => {
+    const weekDays = Array.from({ length: 7 }, (_, index) => startDay + index + 1);
+    return (
+      <div key={startDay} className={styles.week}>
+        {weekDays.map((day) => (
+          <div key={day} className={styles.day}>
+            {day <= daysInMonth ? renderDay(day) : null}
+          </div>
+        ))}
+      </div>
+    );
+  };s
+
   const renderCalendar = () => {
-    const daysArray = Array.from({ length: daysInMonth }, (_, index) => index + 1);
-    return daysArray.map(renderDay);
+    const weeksArray = Array.from({ length: Math.ceil(daysInMonth / 7) }, (_, index) => index * 7);
+    return (
+      <div>
+        {renderDayLabels()}
+        <div className={styles.calendar}>
+          {weeksArray.map((startDay) => renderWeek(startDay))}
+        </div>
+      </div>
+    );
   };
 
   return (
